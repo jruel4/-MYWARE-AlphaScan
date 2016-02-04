de628ac56667c1b0566d90fcf3752b2d40251e45
martianmartin <martinmolina147@gmail.com>
2016-02-03 19:57:25 -0500

    Merged SPIFFS networks parameter storage
    Firmware now checks SPIFFS for network params and tries to connect to
    WAN. If it fails, it enters SoftAP mode to acquire valid parameters.

--- Firmware/AlphaScanFirmware_0_0_0/AlphaScanFirmware_0_0_0.ino
+++ Firmware/AlphaScanFirmware_0_0_0/AlphaScanFirmware_0_0_0.ino
@@ -2,22 +2,25 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <WiFiUDP.h>
+#include "FS.h"
 
 // Define hard coded network constants
 //const char* ssid     = "PHSL2";
 //const char* password = "BSJKMVQ6LF2XH6BJ";
-//const char* host     = "192.168.1.8";
+const char* host     = "192.168.1.8";
+
 char ssid[20];
 char password[50];
-char host[15];
+
 
 String ssid_str;
 String password_str;
-String host_ip_str;
+
+
 
 bool ssid_set = false;
 bool password_set = false;
-bool host_set = false;
+bool host_set = true;
 bool network_set = false;
 
 // TCP constants
@@ -32,6 +35,11 @@ WiFiClient client; // TODO consider using separate client instances for AP and T
 
 // Declare Udp Instance
 WiFiUDP Udp;
+
+// Declare filesystem variables
+const char path[] = "/net_params.txt";
+bool open_a = true;
+File f;
 
 // Declare function prototypes
 void establishHostTCPConn();
@@ -52,32 +60,124 @@ void setup() {
   // Setup SPI
   // Setup WiFi
 
-  //--Connect to host application--//
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Check SPIFFS for network parameters
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  // 1) Use b) stored, c) serial uploaded SSID and Passkey to connect (see: https://github.com/esp8266/Arduino/blob/master/doc/filesystem.md)
+  // Initiate SPIFFS
+  Serial.println("inititating file SPIFFS class");
+  if(SPIFFS.begin())Serial.println("Spiffs mount success");else {Serial.println("Spiffs mount FAIL");return;}
+  
+  // Check to see if net_params file exists
+  Serial.print("checking if exists: ");Serial.println(path);
+  if(SPIFFS.exists(path)) {
+    Serial.println("path exists");
+    open_a = true;
+  }
+  else {
+    Serial.println("path does not exist");
+    open_a = false;
+  }
 
+  // If path exists, try to open file
+  if (open_a) {
+    f = SPIFFS.open(path,"r");
+
+    // Check if file open succeeded
+    if (!f) {
+    Serial.println("file open failed");
+    return; // TODO deal with this properly...
+    }
+    else {
+      Serial.println("file open SUCCESS"); 
+    }
+
+    // Read parameters from file
+    f.seek(0,SeekSet);
+    // Read SSID
+    if(f.available()) {
+      //Lets read line by line from the file
+      ssid_str = f.readStringUntil('\n');
+      ssid_str = extractValue(ssid_str,"ssid");
+      strcpy(ssid,&(ssid_str[0]));
+      ssid_set = true;
+    }
+    
+    // Read password
+    if(f.available()) {
+      //Lets read line by line from the file
+      password_str = f.readStringUntil('\n');
+      password_str = extractValue(password_str,"pass");
+      strcpy(password,&(password_str[0]));
+      password_set = true;
+    }
+
+    f.close();
+    
+    network_set = true;
+  
+  }
+  // else procees to SoftAP Mode
+  else {
+    network_set = false;
+  }
+
+ 
+  
+  
+  
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  
   // acquire SSID and password via SoftAP
   Serial.println("aquire SSID and other network params now...");
   while(!network_set) {
     
     acquireNetworkParams();
-  
+
+    if (network_set) {
+      // Write network params to SPIFFS
+
+      // open file
+      f = SPIFFS.open(path,"w");
+      if (!f) {
+      Serial.println("file open failed");
+      return;
+      }
+      else {
+        Serial.println("file open SUCCESS"); 
+      }
+      
+      // write lines
+      f.seek(0,SeekSet);
+      f.println("ssid_" + ssid_str + "_endssid");
+      f.println("pass_" + password_str + "_endpass");
+      f.close();
+    }
   }
    
   Serial.println("Network parameters acquired, now attempting to join LAN with: ");
-  Serial.println("ssid: " + ssid_str + ", pass: " + password_str + ", host_ip: " + host_ip_str);
+  Serial.println("ssid: " + ssid_str + ", pass: " + password_str );
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(1000);
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  
   
   //WiFi.begin("PHSL2", "BSJKMVQ6LF2XH6BJ");
   WiFi.begin(ssid, password);
 
-  
+  // TODO add counter here, and if does not work after N tries, switch to SoftAP state. Requires state machine
+  int attempts = 0;  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    attempts++;
+
+    if (attempts > 15) { // may want to tune this number up for poor connections
+      Serial.println("WiFi parameters appear to be invalid");
+    }
   }
   Serial.println("");
   Serial.println("WiFi connected");
@@ -109,7 +209,7 @@ void loop() {
 
 void acquireNetworkParams() {
 
-  // Define variables
+  // Define local AP variables
   const char AP_NAME_STR[] = "AlphaScanAP";
   const char WiFiAPPSK[] = "martianwearables";
   String custom_response;
@@ -179,17 +279,9 @@ void acquireNetworkParams() {
     password_set = true;
   }
 
-  // Rx Host IP
-  else if (req.indexOf("host_ip") >= 0) {
-    host_ip_str = extractValue(req,"host_ip");
-    strcpy(host,&(host_ip_str[0]));
-    custom_response = "Host IP";
-    host_set = true;
-  }
-
   // Echo network params
   else if (req.indexOf("echo_params") >= 0) {
-    custom_response = "ssid: " + ssid_str + ", pass: " + password_str + ", host_ip: " + host_ip_str;
+    custom_response = "ssid: " + ssid_str + ", pass: " + password_str;
   }
 
   else {
