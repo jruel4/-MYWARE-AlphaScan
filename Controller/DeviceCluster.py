b354de00a04b9940fd875cfeedb549e91bb78a4c
martianmartin <martinmolina147@gmail.com>
2017-03-02 18:00:04 -0500

    Implemented device class command loop - untested

--- Controller/DeviceCluster.py
+++ Controller/DeviceCluster.py
@@ -12,88 +12,140 @@ class DeviceCluster:
         self.dev = [AlphaScanDevice(50007+i) for i in range(num_devices)]
     
     def open_debug_port(self):
-        pass
+        for d in self.dev:
+            d.open_debug_port()
+        return "unimplemented r-code"
     
     def close_debug_port(self):
-        pass
+        for d in self.dev:
+            d.open_debug_port()
+        return "unimplemented r-code"
     
     def read_debug_port(self):
-        pass
+        for d in self.dev:
+            d.close_debug_port()
+        return "unimplemented r-code"
         
     def init_TCP(self):
-        pass
+        for d in self.dev:
+            d.init_TCP()
+        return "unimplemented r-code"
             
     def close_TCP(self):
         for d in self.dev:
             d.close_TCP()
+        return "unimplemented r-code"
         
     def close_UDP(self):
-        pass
+        for d in self.dev:
+            d.close_UDP()
+        return "unimplemented r-code"
     
     def close_event(self):
-        pass
+        for d in self.dev:
+            d.close_event()
+        return "unimplemented r-code"
                 
     def generic_tcp_command_BYTE(self, cmd, extra = ''):
+        response = ''
         for d in self.dev:
-            d.generic_tcp_command_BYTE("ACC_get_status")
-        return "COOL!"
+            response += d.generic_tcp_command_BYTE("ACC_get_status")
+        return response
         
-    def generic_tcp_command_OPCODE(self, opcode, extra = ''):
-        pass
-    
+    def generic_tcp_command_OPCODE(self, opcode, _extra = ''):
+        for d in self.dev:
+            d.generic_tcp_command_OPCODE(opcode, extra=_extra)
+        return "unimplemented r-code"
+        
     def generic_tcp_command_STRING(self, txt):
-        pass
+        for d in self.dev:
+            d.generic_tcp_command_STRING(txt)
+        return "unimplemented r-code"
         
-    def read_tcp(self, num_bytes=64):
-        pass	
+    def read_tcp(self, _num_bytes=64):
+        for d in self.dev:
+            d.read_tcp(num_bytes=_num_bytes)	
+        return "unimplemented r-code"
 
     def pull_adc_registers(self): 
-        pass
+        for d in self.dev:
+            d.pull_adc_registers()
+        return "unimplemented r-code"
  
     def push_adc_registers(self, RegMap):
-        pass	
+        for d in self.dev:
+            d.push_adc_registers(RegMap)
+        return "unimplemented r-code"
 	
     def sync_adc_registers(self):
-        pass
+        for d in self.dev:
+            d.sync_adc_registers()
+        return "unimplemented r-code"
 
     def initiate_UDP_stream(self):
-        pass
+        for d in self.dev:
+            d.initiate_UDP_stream()
+        return "unimplemented r-code"
         
     def initiate_TCP_stream(self):
-        pass
+        for d in self.dev:
+            d.initiate_TCP_stream()
+        return "unimplemented r-code"
         
     def initiate_TCP_streamX(self):
-        pass
+        for d in self.dev:
+            d.initiate_TCP_streamX()
+        return "unimplemented r-code"
         
     def initiate_TCP_stream_direct(self):
-        pass
+        for d in self.dev:
+            d.initiate_TCP_stream_direct()
+        return "unimplemented r-code"
         
     def terminate_UDP_stream(self):
-        pass
+        for d in self.dev:
+            d.terminate_UDP_stream()
+        return ['NULL' for i in range(5)]
         
     def terminate_TCP_stream(self):
-        pass
+        for d in self.dev:
+            d.terminate_TCP_stream()
+        return "unimplemented r-code"
     
     def flush_TCP(self):
-        pass
+        for d in self.dev:
+            d.flush_TCP()
+        return "unimplemented r-code"
     
     def flush_UDP(self):
-        pass
+        for d in self.dev:
+            d.flush_UDP()
+        return "unimplemented r-code"
     
     def update_adc_registers(self,reg_to_update):
-        pass
+        for d in self.dev:
+            d.update_adc_registers(reg_to_update)
+        return "unimplemented r-code"
 
     def update_command_map(self):
-        pass
+        for d in self.dev:
+            d.update_command_map()
+        return "unimplemented r-code"
         
     def get_drop_rate(self):
-        pass
+        for d in self.dev:
+            d.get_drop_rate()
+        return "unimplemented r-code"
     
     def broadcast_disco_beacon(self):
-        pass
+        for d in self.dev:
+            d.broadcast_disco_beacon()
+        return "unimplemented r-code"
         
     def listen_for_device_beacon(self):   
-        pass
+        for d in self.dev:
+            d.listen_for_device_beacon()
+        return "unimplemented r-code"
         
     def connect_to_device(self):
         r = [False for i in range(self.num_devices)]
@@ -103,19 +155,29 @@ class DeviceCluster:
         return True
         
     def set_udp_delay(self, delay):
-        pass
+        for d in self.dev:
+            d.set_udp_delay(delay)
+        return "unimplemented r-code"
     
     def parse_sys_commands(self):
-        pass
+        for d in self.dev:
+            d.parse_sys_commands()
+        return "unimplemented r-code"
     
     def ADC_send_hex_cmd(self,cmd):
-        pass
+        for d in self.dev:
+            d.ADC_send_hex_cmd(cmd)
+        return "unimplemented r-code"
         
     def count_time_interval(self):
-        pass
+        for d in self.dev:
+            d.count_time_interval()
+        return "unimplemented r-code"
             
     def close_udp_solo(self):
-        pass
+        for d in self.dev:
+            d.close_udp_solo()
+        return "unimplemented r-code"
     
             
             
