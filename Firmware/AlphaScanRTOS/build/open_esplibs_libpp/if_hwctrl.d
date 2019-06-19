2bc8e12207a657e3e10339e66cd50eaf1173468e
John Ruelas <jruel4@yahoo.com>
2019-06-19 17:56:49 -0500

    Got rid of unneeded/old folders

--- Firmware/AlphaScanRTOS/build/open_esplibs_libpp/if_hwctrl.d
+++
@@ -1,3 +0,0 @@
-build/open_esplibs_libpp//if_hwctrl.o: \
- /media/marzipan/Expansion_1/Development/Projects/superhouse_spi_test_build/esp-open-rtos/open_esplibs/libpp/if_hwctrl.c \
- /media/marzipan/Expansion_1/Development/Projects/superhouse_spi_test_build/esp-open-rtos/open_esplibs/include/open_esplibs.h
